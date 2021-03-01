e96604112f52e84b41682246c830353b1dee0517
repl.it user <replituser@example.com>
2021-03-01 12:40:03 +0000

    Added messages to output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print('EA Sports Servers are trash')
 print("First step: git add .")
-print("Second step: git commit -m 'Your message here' ")
+print("Second step: git commit -m \"Your message here\"")
 print("Last step: git push")
