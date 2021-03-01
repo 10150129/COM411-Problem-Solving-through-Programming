f4f7a20832d144a5205de6a8dbf70dfda82756be
repl.it user <replituser@example.com>
2021-03-01 08:55:20 +0000

    Made a comment on EA Servers

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print('EA Sports servers are trash')
+print('EA Sports Servers are trash')
+print("First step: git add .")
+print("Second step: git commit -m 'Your message here' ")
+print("Last step: git push")
