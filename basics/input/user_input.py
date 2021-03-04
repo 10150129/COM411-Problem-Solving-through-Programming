ab9d84d518b34b20010032746908f2ec792a1ae6
repl.it user <replituser@example.com>
2021-03-04 09:57:35 +0000

    Added a new py file

---
+++ basics/input/user_input.py
@@ -0,0 +1,8 @@
+#Ask user to enter their name
+print("What is ypur name human?")
+name=input(0)
+print("It is nice to meet you human",name)
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,1 @@
-print('EA Sports Servers are trash')
-print("First step: git add .")
-print("Second step: git commit -m \"Your message here\"")
-print("Last step: git push")
+print("What is your name?")
