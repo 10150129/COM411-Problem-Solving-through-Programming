d4ea71409999b3ad6d95731c2f79ee8c3120ae31
repl.it user <replituser@example.com>
2021-03-06 15:42:44 +0000

    Added code to demonstrate the use of different data types

---
+++ basics/input/data_type.py
@@ -0,0 +1,14 @@
+# Ask user to enter their name
+print("What is your name?")
+name=input()
+# Ask user to enter their age
+print("How old are you (in years)?")
+age=input()
+# Ask user to enter their height
+print("How tall are you (in meters)?")
+height=input()
+# Ask user to entert heir weight
+print("How much do you weigh (in kilograms)?")
+weight=input()
+int(bmi=weight/height**2)
+print("/")
