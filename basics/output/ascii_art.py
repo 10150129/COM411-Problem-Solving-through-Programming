dbe139cc836168dadc6ebe523abbdf7b7f1b8b88
repl.it user <replituser@example.com>
2021-03-06 14:19:41 +0000

    Added code to display an ascii art robot

--- basics/output/ascii_art.py
+++ basics/output/ascii_art.py
@@ -1,21 +1,15 @@
-print(" ###### ")
-print("# I AM #")
-print("# BEEP #")
-print(" ###### ")
-print("########")
-print("########")
-print("########")
-print(" ###### ")
-
-
+# Display a box
+print("  _   _   ")
+print("   \ /    ")
 print("##########")
 print("#        #")
 print("#        #")
+print("###    ###")
+print("  #    #  ")
+print(" #      # ")
+print("#        #")
+print("#        #")
 print("##########")
-#Please enter a character format
-character=()
-print("Beep's expression is now as follows:")
-print("##########")
-print("#  o  o  #")
-print("#  ----  #")
-print("##########")
+
+
+
