219c45a33545acceab40e09c38947d639aaea16e
repl.it user <replituser@example.com>
2021-03-06 14:36:42 +0000

    Added code to read user input

--- basics/input/user_input.py
+++ basics/input/user_input.py
@@ -1,9 +1,4 @@
 #Ask user to enter their name
 print("What is your name ?")
 name=input()
-print("It is nice to meet you",name)
-print("Nice to meet you" + name +""")
-
-
-
-
+print("Nice to meet you {}." .format(name))
