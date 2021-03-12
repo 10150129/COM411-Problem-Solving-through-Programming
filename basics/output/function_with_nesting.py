202c59919c27ff868b70952d107aa4ab6ce59bb3
repl.it user <replituser@example.com>
2021-03-12 11:02:04 +0000

    function_with_parameters

---
+++ basics/output/function_with_nesting.py
@@ -0,0 +1,17 @@
+#What lies before us?
+#A large boulder.
+#It's time to Run 
+
+#The function 
+def identify():
+  #Ask user the question 
+  print("What lies before us?")
+  response = input()
+
+  #Check and display message 
+  if (response == "A large boulder."):
+    print("It's time to run!")
+  else:
+   print("We will be fine.")
+#Call to function 
+identify()
--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-#What lies before us?
-#A large boulder.
-#It's time to Run 
+#Escape by ("Jumping over")
+#Escape by ("Running around")
+#Escape by ("Going deeper!")
 
-#The function 
-def identify():
-  #Ask user the question 
-  print("What lies before us?")
-  response = input()
+#We cannot escape that way!The boulder is too big!
+#We cannot escape that way!The boulder is moving too fast!
+#That might just work!Let's go deeper!
 
-  #Check and display message 
-  if (response == "A large boulder."):
-    print("It's time to run!")
+#The function with single plan and name is "plan"
+
+def escape_by(plan):
+  if(plan == "Jumping over"):
+    print("We cannot escape that way!The boulder is too big!")
+  elif(plan == "Running around"):
+    print("We cannot escape that way!The boulder is moving too fast!")
+  elif(plan == "Going deeper!"):
+    print("That might just work!Let's go deeper!")
   else:
-   print("We will be fine.")
+    print("Not sure about the plan!")
+
 #Call to function 
-identify()
+
+escape_by ("Jumping over")
+escape_by ("Running around")
+escape_by ("Going deeper")
+escape_by ("Let's listen")
+
 
 
 
