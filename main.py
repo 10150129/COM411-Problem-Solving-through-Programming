5cc90d2aecb5b56d76c0b890d8e8ad737cad6503
repl.it user <replituser@example.com>
2021-03-16 11:52:15 +0000

    How to add items to a list using code

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-#Define an empy list
-fruits = []
+#Piotr's code - how to add to the list
 
-#Define a list with items
-vegetables = ["Carrot", "Peas"]
+def get_fruits():
+  fruits = []
+  print("How many fruits would you like to enter?")
+  n = int(input())
+  for i in range(4):
+    print("Type in the next fruit:")
+    fruits.append(input())
+ 
+  #Print all items
+  print("Your fruits are {}".format(fruits))
 
-#Add iems to the list
-fruits.append("Apple")
-fruits.append("Banana")
-fruits.append("Tomatoe")
-fruits.append("Banana")
-print(fruits)
+  #Print only few items by slicing 
+  print(f"Sliced list: {fruits[0:5]}")
 
-#Remove an item from a list
-fruits.remove("Banana")
-print(fruits)   
+  #Print only few items by using negative index
+  print(f"Negative index:{fruits[-2:0]}")
 
-#Remove item by index
-del fruits[1]
-print(fruits)
+get_fruits()
 
-#Insert a value not at the end
-fruits.insert(1,"Pineapple")
-print(fruits)
 
-#Access single element
-print(fruits[0])
 
