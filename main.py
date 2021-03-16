e649309893c609f00ecab61b5168cad735622b9b
repl.it user <replituser@example.com>
2021-03-16 10:28:51 +0000

    Prime numbers in coding

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
-def display_box(name):
-  message = "* Hello {} *".format(name)
-  print("*" *(len(message)))
-  print(message)
-  print("*" *(len(message)))
+def isPrime(x):
+  for i in range(2,x):
+    if x%i == 0:
+      return False
+  return True
 
-def greet_user():
-  print("Please enter your name")
-  name = input()
-  display_box(name)
+def findPrime(begining,finish):
+  for j in range(begining, finish):
+    if isPrime(j):
+      return j
 
-greet_user()
-    
+def encrypt():
+  print("Provide two integers")
+  x = int(input())
+  y = int(input())
+  prime1 = findPrime(x,y)
+  print("Provide two more integers")
+  x = int(input())
+  y = int(input())
+  prime2 = findPrime(x,y)
+  return prime1*prime2
 
+print(encrypt (), encrypt())
+
+
+
+x=32
+y=35
+
+print(f"The prime between {x} and {y} is {findPrime(x,y)}")
+
+
+print("What is the number?")
+x=int(input())
+if isPrime(x):
+  print(f"The number {x} is prime!")
+else:
+  print("The number was not prime!")  
 
 
 
