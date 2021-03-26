65a5733331da994052df15615090c2c19fae17b0
repl.it user <replituser@example.com>
2021-03-26 09:48:53 +0000

    added code to demonstrate sets

---
+++ Demos/sets.py
@@ -0,0 +1,27 @@
+ #Piotr's code for Sets 
+
+#initialise an empty set 
+colours = set()
+print(type(colours))
+
+#initialise non-empty set 
+colors = {"blue", "red", "yellow"}
+print(type(colors))
+print(colors)
+
+#adding elements to a set 
+colors.add("purple")
+colours.add("red")
+colours.add("black")
+colours.add("green")
+
+print(colours)
+print(colors)
+
+#union - joining two sets  
+set1 = colours.union(colors)
+print(set1)
+
+#intersection - finding common elements  
+set2 = colours.intersection(colors)
+print(set2)
--- main.py
+++ main.py
@@ -1,30 +1,12 @@
- #Piotr's code for Sets 
 
-#initialise an empty set 
-colours = set()
-print(type(colours))
+def observed():
+  observations = {"Flying car", "Skye scraper", "Laser", "Dome"}
+  return observations
 
-#initialise non-empty set 
-colors = {"blue", "red", "yellow"}
-#print(type(colors))
-#print(colors)
-
-#adding elements to a set 
-colors.add("purple")
-colours.add("red")
-colours.add("black")
-colours.add("green")
-
-print(colours)
-print(colors)
-
-#union - joining two sets  
-set1 = colours.union(colors)
-print(set1)
-
-#intersection - finding common elements  
-set2 = colours.intersection(colors)
-print(set2)
+def run():
+  print(observed())
+  
+run()
 
 
 
