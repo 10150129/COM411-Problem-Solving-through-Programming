ef510f03e5f09f3440338fab931c5931cf96c850
repl.it user <replituser@example.com>
2021-04-27 09:32:07 +0000

    Data visualisation in Matplotlib

---
+++ basics/Demos/week7_data_files.py
@@ -0,0 +1,36 @@
+#Piotr's code on how to creat a File 
+#going to open a File 
+#file = open("gaga.txt", "a")
+
+#file.write("\nsdghjkhhjjhgk")
+
+#file.close
+
+#going to create a list of lines 
+#lista = file.readlines()
+
+#print(lista)
+
+#for line in lista:
+  #print(line, end = "")
+#print(file.readline(100), end="")  
+
+#How to access data from File 
+name = input("Enter file name")
+name += ".txt"
+
+f1 = open("john.txt")
+f2 = open("harry.txt")
+
+for i in range(3):
+  print(f"\033[92mJohn: {f1.readline()}\033[0m", end="")
+  print(f"\033[93mHarry: {f2.readline()}\033[0m", end="")
+  #the \033[92m will change john lines green
+  #\033[93m will make harry lines yellow
+
+f1, f2.close()
+
+
+
+
+
---
+++ basics/Demos/week8_data_visualisation.py
@@ -0,0 +1,14 @@
+#Piotr's code on matplotlib, how to visualize data in matplotlib
+#plt.plot
+
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5,6,7]
+y = [10,15.5,18.2,20,25,36,40]
+
+plt.xlabel("Age")
+plt.ylabel("Score")
+
+plt.plot(x,y,"mD")
+
+plt.show()
