28b4c59509baa87de95a06550b9b7b0d367e17c2
repl.it user <replituser@example.com>
2021-05-07 09:59:36 +0000

    week9 animation with matplotlib

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-#Code on visual animations
+#Code on visual animations with matplotlib week9 animation with matplotlib activity 2
 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
 
+fig, ax =plt.sublots()
 
 def animate(frame):
-  print(f"Frame:{frame}")
+  global ax
+  ax.set_xlimit(0,10)
+  ax.set_ylim(0,10)
+  ax.plot(frame,frame)
+  return frame
 
 def run():
-  fig, ax=plt.sublot()
-  simple_animation=animation.FuncAnimation(fig, animate, frames = 10, interval = 1000)
+  global fig 
+  simple_animation=animation.FuncAnimation(fig, animate, frames=10, interval=1000)
   plt.show()
 
 run()  
-
-
